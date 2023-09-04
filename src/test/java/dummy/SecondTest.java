c06d93f4351018ed6853764fd57b654d154dfc1e
Admin <Admin@DESKTOP-OF75GGU>
2023-09-04 10:54:26 +0530

    aaaa

---
+++ src/test/java/dummy/SecondTest.java
@@ -0,0 +1,13 @@
+package dummy;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SecondTest {
+	@Test
+	public void test()
+	{
+		Reporter.log("secondtest");
+	}
+
+}
