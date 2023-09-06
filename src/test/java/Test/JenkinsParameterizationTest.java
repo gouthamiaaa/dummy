a0e8e6708fc7ed278efaf9f80ab42f6cb6de292c
Admin <Admin@DESKTOP-OF75GGU>
2023-09-06 08:56:59 +0530

    &&

---
+++ src/test/java/Test/JenkinsParameterizationTest.java
@@ -0,0 +1,33 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class JenkinsParameterizationTest {
+@Test
+public void demo() throws InterruptedException {
+	WebDriver driver=null;
+	
+	String browser=System.getProperty("browser");
+	switch(browser) {
+	case "chrome":
+	
+        System.setProperty("webDriver.chrome.driver","./src/main/resources/chromedriver.exe");
+	    driver=new ChromeDriver();
+	    break;
+	case "firefox":
+		driver=new FirefoxDriver();
+		break;
+	case "edge":
+		driver=new EdgeDriver();
+	     break;
+	default:
+	System.out.println("invalid browser info");
+	}
+	Thread.sleep(2000);
+	driver.quit();
+}
+}
